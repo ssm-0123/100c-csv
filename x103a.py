c69601dfd20338f78283b8e1b2fb1bd045f61949
Chris Yang <cyang@deltalearns.ca>
2021-12-10 14:49:06 -0800

    first update

---
+++ x103a.py
@@ -0,0 +1,22 @@
+#!python3
+
+"""
+Create a function called getData()
+getData takes no input parameters
+return: file contents as a string
+
+"""
+def getData():
+    return data
+
+def main():
+    data = getData()
+    assert "list" in str(type(db))
+    assert "list" in str(type(db[0]))
+    assert data.find("No.,Equipment Item N") == 0
+    print(f"Barry is found at position: {data.find('Barry')}")
+    assert data.find("Barry") == 27089
+
+if __name__ == "__main__":
+    main()
+
